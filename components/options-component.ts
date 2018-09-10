111bbec1733c2c47b219f588105d5515897748f8
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2018-09-10 10:23:31 +0300

    feat(options): add tags and params options

--- components/options-component.ts
+++ components/options-component.ts
@@ -12,13 +12,28 @@ export class OptComponent extends OptionsComponent {
             short: Constants.SHORT_CONVERT_COMMAND,
             help: 'Specifies the directory where the json files have to be generated.',
         };
+
         const generateFromOption: DeclarationOption = {
             name: Constants.RENDER_COMMAND,
             short: Constants.SHORT_RENDER_COMMAND,
             help: 'Specify from where to get the loclized json data.'
         }
 
+        const includeTagsOption: DeclarationOption = {
+            name: Constants.TAGS_INCLUDE_COMMAND,
+            help: 'Specify whether to include tags per comment.',
+            defaultValue: true
+        }        
+
+        const includeParamsOption: DeclarationOption = {
+            name: Constants.PARAMS_INCLUDE_COMMAND,
+            help: 'Specify whether to include params per comment.',
+            defaultValue: true
+        }
+
         this.application.options.addDeclaration(new OptionDeclaration(generateToOption));
         this.application.options.addDeclaration(new OptionDeclaration(generateFromOption));
+        this.application.options.addDeclaration(new OptionDeclaration(includeTagsOption));
+        this.application.options.addDeclaration(new OptionDeclaration(includeParamsOption));
     }
 }
--- utils/constants.ts
+++ utils/constants.ts
@@ -3,10 +3,13 @@ export class Constants {
     static readonly TEXT = 'text';
     static readonly SHORT_TEXT = 'shortText';
 
+    // Commands
     static readonly CONVERT_COMMAND = 'generate-json';
     static readonly RENDER_COMMAND = 'generate-from-json';
     static readonly SHORT_CONVERT_COMMAND = 'gen-json';
     static readonly SHORT_RENDER_COMMAND = 'gen-from-json';
+    static readonly TAGS_INCLUDE_COMMAND = 'tags';
+    static readonly PARAMS_INCLUDE_COMMAND = 'params';
 
     static readonly GLOBAL_FUNCS_FILE_NAME = 'globalFunctions';
 }
