f4f9d431a7c378777575a9db8523340791fb2f97
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2018-10-03 16:59:04 +0300

    feat(*): add helper localization function

--- components/options-component.ts
+++ components/options-component.ts
@@ -1,41 +1,54 @@
 import { Component } from 'typedoc/dist/lib/output/components'
 import { OptionsComponent, DiscoverEvent } from 'typedoc/dist/lib/utils/options/options'
-import { OptionDeclaration, DeclarationOption, ParameterType, ParameterHint } from 'typedoc/dist/lib/utils/options/declaration';
+import { OptionDeclaration, DeclarationOption, ParameterType } from 'typedoc/dist/lib/utils/options/declaration';
 import { Option } from 'typedoc/dist/lib/utils/component';
 import { Constants } from '../utils/constants';
 
 @Component({ name: 'options-component' })
 export class OptComponent extends OptionsComponent {
-    public initialize() {            
+    public initialize() {
         const generateToOption: DeclarationOption = {
-            name: Constants.CONVERT_COMMAND,
-            short: Constants.SHORT_CONVERT_COMMAND,
+            name: Constants.CONVERT_OPTION,
+            short: Constants.SHORT_CONVERT_OPTION,
             help: 'Specifies the directory where the json files have to be generated.',
         };
 
         const generateFromOption: DeclarationOption = {
-            name: Constants.RENDER_COMMAND,
-            short: Constants.SHORT_RENDER_COMMAND,
+            name: Constants.RENDER_OPTION,
+            short: Constants.SHORT_RENDER_OPTION,
             help: 'Specify from where to get the loclized json data.'
         }
 
         const includeTagsOption: DeclarationOption = {
-            name: Constants.INCLUDE_TAGS_COMMAND,
+            name: Constants.INCLUDE_TAGS_OPTION,
             help: 'Specify whether to include tags per comment.',
             type: ParameterType.Boolean,
             defaultValue: true
         }        
 
         const includeParamsOption: DeclarationOption = {
-            name: Constants.INCLUDE_PARAMS_COMMAND,
+            name: Constants.INCLUDE_PARAMS_OPTION,
             help: 'Specify whether to include params per comment.',
             type: ParameterType.Boolean,
             defaultValue: true
         }
 
-        this.application.options.addDeclaration(new OptionDeclaration(generateToOption));
-        this.application.options.addDeclaration(new OptionDeclaration(generateFromOption));
-        this.application.options.addDeclaration(new OptionDeclaration(includeTagsOption));
-        this.application.options.addDeclaration(new OptionDeclaration(includeParamsOption));
+        const localizeOption: DeclarationOption = {
+            name: Constants.LOCALIZE_OPTION,
+            help: 'Specify your localization for instance (jp)'
+        }
+
+        const shellStringsOption: DeclarationOption = {
+            name: Constants.TEMPLATE_STRINGS_OPTION,
+            help: 'Path to the json file which contains your localized template strings'
+        }
+
+        this.application.options.addDeclarations([
+            generateToOption, 
+            generateFromOption, 
+            includeTagsOption, 
+            includeParamsOption,
+            localizeOption,
+            shellStringsOption]);
     }
 }
